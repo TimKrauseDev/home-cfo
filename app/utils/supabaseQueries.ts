288f039636ffbed957916625fe72e9388c469198
Tim Krause <timkrausedev@gmail.com>
2025-12-09 09:20:00 -0500

    ops, update supabaseQueries

--- app/utils/supabaseQueries.ts
+++ app/utils/supabaseQueries.ts
@@ -1,9 +1,38 @@
 import { useSupabase } from '@/composables/supabaseClient'
 import type { QueryData } from '@supabase/supabase-js'
 
-export const getInstrumentsQuery = () => {
-  const supabase = useSupabase()
-  return supabase.from('instruments').select()
-}
+// export const getInstrumentsQuery = () => {
+//   const supabase = useSupabase();
+//   return supabase.from('instruments').select()
+// }
 
-export type Instruments = QueryData<ReturnType<typeof getInstrumentsQuery>>
+// export type Instruments = QueryData<ReturnType<typeof getInstrumentsQuery>>
+
+/** Get All Projects */
+export const getProjectsQuery = () => {
+  const supabase = useSupabase()
+  return supabase.from('projects').select('*')
+}
+export type Projects = QueryData<ReturnType<typeof getProjectsQuery>>
+
+/** Get Single Project by ID */
+export const getProjectQuery = (id: string) => {
+  const supabase = useSupabase()
+  return supabase.from('projects').select('*').eq('id', id).single()
+}
+export type Project = QueryData<ReturnType<typeof getProjectQuery>>
+
+/** Update Project by ID */
+export const updateProjectQuery = (id: string, data: Project) => {
+  console.log('Updating project with data:', data)
+  const supabase = useSupabase()
+  return supabase.from('projects').update({ ...data, updated_at: new Date().toISOString() }).eq('id', id)
+}
+export type UpdateProject = QueryData<ReturnType<typeof updateProjectQuery>>
+
+/** Delete Project by ID */
+export const deleteProjectQuery = (id: string) => {
+  const supabase = useSupabase()
+  return supabase.from('projects').delete().eq('id', id)
+}
+export type DeleteProject = QueryData<ReturnType<typeof deleteProjectQuery>>
