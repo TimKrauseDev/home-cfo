297b054b93edf6f039186bd65772082f96f5a1c6
Tim Krause <timkrausedev@gmail.com>
2025-12-10 15:38:27 -0500

    gave in and fixed eslint issue

--- app/utils/dateUtils.ts
+++ app/utils/dateUtils.ts
@@ -1,19 +1,20 @@
+/* eslint-disable @typescript-eslint/no-extraneous-class */
 import { CalendarDate } from '@internationalized/date'
 
 export class DateUtils {
-  static validateYear (year: number): boolean {
+  static validateYear(year: number): boolean {
     return year >= 1900 && year <= 2100
   }
 
-  static calendarDateToUTCDate (date: CalendarDate): Date {
+  static calendarDateToUTCDate(date: CalendarDate): Date {
     return new Date(Date.UTC(date.year, date.month - 1, date.day))
   }
 
-  static calendarDateToUTCDateISO (date: CalendarDate): string {
+  static calendarDateToUTCDateISO(date: CalendarDate): string {
     return new Date(Date.UTC(date.year, date.month - 1, date.day)).toISOString()
   }
 
-  static UTCDateISOToCalendarDate (isoString: string): CalendarDate {
+  static UTCDateISOToCalendarDate(isoString: string): CalendarDate {
     const date = new Date(isoString)
     return new CalendarDate(
       date.getUTCFullYear(),
@@ -22,13 +23,12 @@ export class DateUtils {
     )
   }
 
-  static tableColumnDateFormat (isoString: string): string {
+  static tableColumnDateFormat(isoString: string): string {
     if (!isoString) return ''
 
     return new Date(isoString).toLocaleDateString('en-US', {
       dateStyle: 'medium',
       timeZone: 'UTC'
     })
-
   }
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,8 +4,6 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 export default withNuxt({
   rules: {
     'vue/no-multiple-template-root': 'off',
-    'vue/max-attributes-per-line': ['error', { singleline: 3 }],
-    'vue/space-before-function-paren': 'off',
-    'space-before-function-paren': 'off'
+    'vue/max-attributes-per-line': ['error', { singleline: 3 }]
   }
 })
